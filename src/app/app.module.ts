100e888776de2744763dab7434d99d42fb73bd84
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-08-19 21:38:26 +0300

    new service githab added'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
@@ -12,7 +13,8 @@ import { SearchformComponent } from './searchform/searchform.component';
     SearchformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
