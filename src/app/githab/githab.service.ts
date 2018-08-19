1916d7c5ec6ec065f0bf54eb383fbb56289670af
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-08-19 22:58:01 +0300

    Bootstrap added

--- src/app/githab/githab.service.ts
+++ src/app/githab/githab.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+//import {GithabService} from '../githab/githab.service.spec';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithabService {
+  githabService = GithabService;
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+    //this.githabService = new GithabService ("","","","");
+   }
 }
